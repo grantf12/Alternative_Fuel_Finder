76cb4a9ca095b334a32484118871119793712709
Caspento <Mrcaspento@gmail.com>
2020-11-16 17:50:25 -0500

    javascript

---
+++ script.js
@@ -0,0 +1,17 @@
+let apiKey ='3037486a16mshc6512bad445d222p100853jsnc25d6486900b'
+
+
+
+
+
+
+
+function makeFuelList(){
+    let = $('<li>','<button>').addClass('btn list-group-item').text(station);
+    $('list').append(listItem)
+};
+
+$("#exampleBtn").click(function(){
+    event.preventDefault();
+
+})
