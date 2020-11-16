905bad6cb24a8e87e4909864aa86f4bee16ec453
Caspento <Mrcaspento@gmail.com>
2020-11-16 18:01:59 -0500

    templete js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let apiKey ='3037486a16mshc6512bad445d222p100853jsnc25d6486900b'
 
 
 
-
+var fuelOutPut = $('#example').val()
 
 
 
@@ -11,7 +11,22 @@ function makeFuelList(){
     $('list').append(listItem)
 };
 
+
+//========================================================
+//Section of code below is for the user input to be displayed on the dashboard listed
+//=======================================================
+//the submit button
 $("#exampleBtn").click(function(){
     event.preventDefault();
+    example = $('#example').val();
+    $('#example').val("");
 
+    const queryUrl = "https://community-nrel-national-renewable-energy-laboratory.p.rapidapi.com/api/alt-fuel-stations/v1.json?state=North%20Carolina&zip=28217&fuel_type=E85%2CELEC&limit=2"
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+      }).then(function (response) {
+          console.log(response);
+      })
 })
