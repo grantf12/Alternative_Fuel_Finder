9b72949c96beb4d45b8ab210e8eb738673482eef
Lindsay Morris <lindsaymorris813@gmail.com>
2020-11-17 16:53:27 -0500

    modal additions

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
 let apiKey ='3037486a16mshc6512bad445d222p100853jsnc25d6486900b'
-
-
-
+let appID = 'wu2NZ7wMWa5kcBJ2Okrx'
+let fuelAppApiKey = 'rBMnSNgmY86eyVSBI1tFCJ7G6J5rC3OcsyPwL7xOvIw'
 var fuelOutPut = $('#example').val()
+let userPosition = $("#result").val() 
 
-$("#select-vehicle-type").on("change", function() {
-    if ($("#select-vehicle-type : selected") === "gas"){
-        $("#gas-container").attr("class", "select is-rounded is-dark is-large");
-    } else {
-        $("#gas-container").attr("class", "select is-rounded is-dark is-large is-hidden")
-    }
+
+//========================================================================
+// Geo Location functions
+if("geolocation" in navigator){
+    navigator.geolocation.getCurrentPosition(function(position){
+        console.log("Lat:" +position.coords.latitude+ "Lang :" + position.coords.longitude);
+    });
+}else{
+    console.log('ERROR 404')
 }
+//
+//$("#sumbit-type").click(function () { //user clicks button
+//	if ("geolocation" in navigator){ //check geolocation available 
+//		//try to get user current location using getCurrentPosition() method
+//		navigator.geolocation.getCurrentPosition(function(position){ 
+//                $("#result").html("Found your location <br />Lat : "+position.coords.latitude+" </br>Lang :"+ position.//coords.longitude);
+//			});
+//	}else{
+//		console.log("Browser doesn't support geolocation!");
+//	}
+//});
 
 function makeFuelList(){
     let = $('<li>','<button>').addClass('btn list-group-item').text(station);
     $('list').append(listItem)
 };
 
+$("#select-vehicle-type").on("change", function(){
+    if ($("#select-vehicle-type :selected").val() === "gas") {
+        $("#gas-container").attr("class", "container m-3 mt-5 has-text-centered");
+    } else {
+        $("#gas-container").attr("class", "container m-3 mt-5 has-text-centered is-hidden");
+    }
+})
 
 //========================================================
 //Section of code below is for the user input to be displayed on the dashboard listed
@@ -34,6 +55,9 @@ $("#exampleBtn").click(function(){
         method: "GET"
       }).then(function (response) {
           console.log(response);
+
+
       })
-    makeFuelList();
 })
+
+
