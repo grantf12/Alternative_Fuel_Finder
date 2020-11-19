2fdac0ec6f54c2094454ef6ff47e16311fd171d4
Lindsay Morris <lindsaymorris813@gmail.com>
2020-11-19 11:21:38 -0500

    removed unnecessary JS

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //Global Vars
-let apiKey ='3037486a16mshc6512bad445d222p100853jsnc25d6486900b'
+let apiKey = '3037486a16mshc6512bad445d222p100853jsnc25d6486900b'
 let appID = 'wu2NZ7wMWa5kcBJ2Okrx'
 let fuelAppApiKey = 'rBMnSNgmY86eyVSBI1tFCJ7G6J5rC3OcsyPwL7xOvIw'
 var fuelOutPut = $('#example').val()
-let userPosition = $("#result").val() 
+let userPosition = $("#result").val()
 
 //Json web token credentials
 let JSONwebToken = '0LUvCafeaQ4taVT6uNYleX6pNyF37UpsNJWgDabOXfUjUWcoXJ9m48ZT8M5ZWTMDSHcmFWmwjfHvuB7kBXSatw'
@@ -21,25 +21,6 @@ var longitude;
 // Geo location variables
 // to retrieve current user postion
 
-// var userpostion = getLocation();
-// //========================================================================
-// Geo Location functions
-//=======================================================
-function geolocation(position) {
-    navigator.geolocation.getCurrentPosition(function(position){
-    console.log("Lat:" +position.coords.latitude+ "Lang :" + position.coords.longitude);
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
-})}
-
-//=======================================================
-//Global Vars
-//=======================================================
-//Json web token credentials
-
-// Geo location variables
-// to retrieve current user postion
-
 //========================================================================
 // Geo Location functions
 //=======================================================
@@ -47,23 +28,15 @@ function geolocation(position) {
 function getLocation() {
     return new Promise(function (resolve, reject) {
         navigator.geolocation.getCurrentPosition(resolve, reject);
-      });
-    }
-  
-function showPosition(position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
-    console.log(latitude);
-    console.log(longitude);
-    console.log(position.coords.latitude + "" + position.coords.longitude);
+    });
 }
 
 $("#select-vehicle-type").on("change", function () {
     if ($("#select-vehicle-type :selected").val() === "gas") {
         $("#gas-container").attr("class", "container mt-5 has-text-centered");
-    }else if($("#select-vehicle-type :selected").val() === "electric") {
-        $('#evConnector-container').attr("class","container mt-5 has-text-centered");
-        $('#evChargerType-container').attr("class","container mt-5 has-text-centered");
+    } else if ($("#select-vehicle-type :selected").val() === "electric") {
+        $('#evConnector-container').attr("class", "container mt-5 has-text-centered");
+        $('#evChargerType-container').attr("class", "container mt-5 has-text-centered");
         $("#gas-container").attr("class", "container mt-5 has-text-centered is-hidden");
     }
 })
@@ -74,8 +47,8 @@ $("#select-vehicle-type").on("change", function () {
 //the submit button
 
 //open modal
-$("#submit-button").on("click", function(){
-    var radius= $("#select-radius :selected").val();
+$("#submit-button").on("click", function () {
+    var radius = $("#select-radius :selected").val();
     if (radius === "Select search radius") {
         $("#select-radius").attr("class", "select is-rounded is-danger is-large is-focused");
         $("#select-radius").append('<span class="icon is-small is-left"><i class="fas fa-exclamation-triangle"></i> </span>');
@@ -86,40 +59,40 @@ $("#submit-button").on("click", function(){
     electricInfo();
 })
 
-$("#current-location").on("click", function(){
+$("#current-location").on("click", function () {
     getLocation()
-    .then((position) => {
-        console.log(position);
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude;
-        console.log(latitude);
-        console.log(longitude);
-        $("#address").attr("placeholder", latitude + ", " + longitude);
-      })
+        .then((position) => {
+            console.log(position);
+            latitude = position.coords.latitude;
+            longitude = position.coords.longitude;
+            console.log(latitude);
+            console.log(longitude);
+            $("#address").attr("placeholder", latitude + ", " + longitude);
+        })
 })
 //close modal
-$("#close-modal").on("click", function(){
+$("#close-modal").on("click", function () {
     $("#results").attr("class", "modal");
 })
 
 function electricInfo() {
-    var electricAPIKey= "Th9TbtOCXmrJhKEo2F7cW2Srorv25I70XaPcviiw";
+    var electricAPIKey = "Th9TbtOCXmrJhKEo2F7cW2Srorv25I70XaPcviiw";
     var electricLocation = encodeURI($("#address").val());
-    var radius= $("#select-radius :selected").val();
-    
+    var radius = $("#select-radius :selected").val();
+
     if (electricLocation !== "") {
         console.log(electricLocation);
-    var electricQueryURL= "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=" + electricAPIKey + "&location=" + electricLocation +  "&radius=" + radius+ "&fuel_type=ELEC&limit=10";
+        var electricQueryURL = "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=" + electricAPIKey + "&location=" + electricLocation + "&radius=" + radius + "&fuel_type=ELEC&limit=10";
     } else {
-        electricQueryURL= "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=" + electricAPIKey + "&latitude=" + latitude + "&longitude=" + longitude + "&radius=" + radius+ "&fuel_type=ELEC&limit=10";
+        electricQueryURL = "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=" + electricAPIKey + "&latitude=" + latitude + "&longitude=" + longitude + "&radius=" + radius + "&fuel_type=ELEC&limit=10";
     }
     $.ajax({
         url: electricQueryURL,
         method: "GET"
-    }).then(function(electricResponse) {
+    }).then(function (electricResponse) {
         console.log(electricResponse)
-        for (i=0; i < electricResponse.fuel_stations.length; i++){
-            
+        for (i = 0; i < electricResponse.fuel_stations.length; i++) {
+
             // Variables that are being pulled
             var fuelStation = electricResponse.fuel_stations[i]
             var stationName = fuelStation.station_name;
@@ -130,27 +103,27 @@ function electricInfo() {
             console.log(stationAddress)
             console.log(evType)
             console.log(howFar)
-            
-            
+
+
             // Variables that need to be made and appended
             var container1 = $('<div class="container p-3 mt-1"></div>')
-                var row = $('<div class="row"></div>')
-                    var col1 = $('<div class="columns"></div>')
-                        var col2 = $('<div class="column is-8"></div>')
-                            var title = $('<div class="subtitle" id="name"></div>')
-                                $(title).text(stationName);
-                            var addy = $('<p class="row h3" id="address"></p>')
-                                $(addy).text(stationAddress);
-                        var col3 = $('<div class="column is-4"></div>')
-                            var EV = $('<p class="row has-text-weight-bold has-text-right">EV Type:</p>')
-                            var EV2 = $('<p class="has-text-right" id="EVType"></p>') 
-                                $(EV2).text(evType);
-                        var row2 = $('<div class="row"></div>')
-                            var distanceTitle = $('<p class="row has-text-weight-bold has-text-right">Distance:</p>')
-                            var distanceAmount = $('<p class="has-text-right" id="goingTheDistance"></p>')
-                                $(distanceAmount).text(howFar.toFixed(2) + "mi");
-                var split = $('<header class="card-header"></header>');
-            
+            var row = $('<div class="row"></div>')
+            var col1 = $('<div class="columns"></div>')
+            var col2 = $('<div class="column is-8"></div>')
+            var title = $('<div class="subtitle" id="name"></div>')
+            $(title).text(stationName);
+            var addy = $('<p class="row h3" id="address"></p>')
+            $(addy).text(stationAddress);
+            var col3 = $('<div class="column is-4"></div>')
+            var EV = $('<p class="row has-text-weight-bold has-text-right">EV Type:</p>')
+            var EV2 = $('<p class="has-text-right" id="EVType"></p>')
+            $(EV2).text(evType);
+            var row2 = $('<div class="row"></div>')
+            var distanceTitle = $('<p class="row has-text-weight-bold has-text-right">Distance:</p>')
+            var distanceAmount = $('<p class="has-text-right" id="goingTheDistance"></p>')
+            $(distanceAmount).text(howFar.toFixed(2) + "mi");
+            var split = $('<header class="card-header"></header>');
+
             // Appending each var to a card-body then append to #modal-card
             $(container1).append(row);
             $(row).append(col1);
@@ -173,7 +146,7 @@ function pricingInfo() {
     $.ajax({
         url: priceURL,
         method: "GET"
-    }).then(function(priceResponse) {
+    }).then(function (priceResponse) {
         console.log(priceResponse);
     })
 
