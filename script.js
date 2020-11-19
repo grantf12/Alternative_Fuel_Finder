dd7f9fea85b49d9f2d99acc6273448eb6da9ee6a
Grant <grantferment@gmail.com>
2020-11-18 19:16:38 -0500

    added dynamically generated results to modal

--- script.js
+++ script.js
@@ -33,29 +33,7 @@ if("geolocation" in navigator){
 }else{
     console.log('ERROR 404')
 }
-//
-//$("#sumbit-type").click(function () { //user clicks button
-//	if ("geolocation" in navigator){ //check geolocation available 
-//		//try to get user current location using getCurrentPosition() method
-//		navigator.geolocation.getCurrentPosition(function(position){ 
-//                $("#result").html("Found your location <br />Lat : "+position.coords.latitude+" </br>Lang :"+ position.//coords.longitude);
-//			});
-//	}else{
-//		console.log("Browser doesn't support geolocation!");
-//	}
-//});
-//=======================================================
-//Global Vars
-//=======================================================
-//Json web token credentials
-let JSONwebToken = '0LUvCafeaQ4taVT6uNYleX6pNyF37UpsNJWgDabOXfUjUWcoXJ9m48ZT8M5ZWTMDSHcmFWmwjfHvuB7kBXSatw'
-let accessKeyId = 'JPP0pPukzSdRomRRqvuxgg'
-// Rest Api credentails
-let restAppId = "sQCEUM1tDeCqiMmQGE2X"
-let restApiKey = "Sk529ATwSKfY2Ms96plU1rkpHasGvgXHnjMUlEzu0HY"
-//Javascript Here credentails
-let jsAppId = "ZDmjBoiHdg5JILLoGycJ"
-let jsApiKey = "Q13x91mhppBgmOOFhI0rpSjimt9kHezCQclpMEpKtbE"
+
 
 
 // Geo location variables
@@ -130,12 +108,52 @@ function electricInfo() {
     var electricAPIKey= "Th9TbtOCXmrJhKEo2F7cW2Srorv25I70XaPcviiw";
     var electricLocation = $("#address").val().trim();
     var radius= $("#select-radius :selected").val();
-    var electricQueryURL= "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=" + electricAPIKey + "&latitude=" + latitude + "&longitude=" + longitude + "&radius=" + radius+ "&fuel_type=ELEC&limit=1";
+    var electricQueryURL= "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=" + electricAPIKey + "&latitude=" + latitude + "&longitude=" + longitude + "&radius=" + radius+ "&fuel_type=ELEC&limit=10";
     console.log(electricQueryURL);
     $.ajax({
         url: electricQueryURL,
         method: "GET"
     }).then(function(electricResponse) {
         console.log(electricResponse)
-    })
+        for (i=0; i < electricResponse.fuel_stations.length; i++){
+            
+            // Variables that are being pulled
+            var fuelStation = electricResponse.fuel_stations[i]
+            var stationName = fuelStation.station_name;
+            var stationAddress = fuelStation.street_address
+            var evType = fuelStation.ev_connector_types[0]
+            var howFar = fuelStation.distance
+            console.log(stationName)
+            console.log(stationAddress)
+            console.log(evType)
+            console.log(howFar)
+            
+            
+            // Variables that need to be made and appended
+                var row = $('<div class="row"></div>')
+                    var col1 = $('<div class="columns"></div>')
+                        var col2 = $('<div class="column is-8"></div>')
+                            var title = $('<div class="title" id="name"></div>')
+                                $(title).text(stationName);
+                            var addy = $('<p class="row subtitle" id="address"></p>')
+                                $(addy).text(stationAddress)
+                        var col3 = $('<div class="column is-4"></div>')
+                            var EV = $('<p class="row has-text-weight-bold has-text-right">EV Type:</p>')
+                            var EV2 = $('<p class="has-text-right" id="EVType"></p>') 
+                                $(EV2).text(evType);
+                        var row2 = $('<div class="row"</div>')
+                            var distanceTitle = $('<p class="row has-text-weight-bold has-text-right">Distance:</p>')
+                            var distanceAmount = $('<p class="has-text-right" id="goingTheDistance"></p>')
+                                $(distanceAmount).text(howFar)
+            
+            // Appending each var to a card-body then append to #modal-card
+            $(row).append(col1);
+            $(col1).append(col2);
+            $(col2).append(title, addy, col3);
+            $(col3).append(EV, EV2, row2, distanceTitle, distanceAmount);
+            $('#modal-card').append(row);
+
+        }
+    }
+    )
 }
